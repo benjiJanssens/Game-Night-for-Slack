f4a7f29b0f48ddc7c187d89e4bd7d03979629d1a
Benjamin Janssens <benji.janssens@gmail.com>
2021-05-03 21:58:58 +0200

    ✨ Added Bolt and dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log("🎲 Game Night for Slack is running. Enjoy your game nights!");
+})();
